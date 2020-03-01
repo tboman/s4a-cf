cb00c4e2da5ab5aa9dbb03c8f0e9139bf358f6ef
tboman <tboman@ionetix.com>
2020-03-01 01:24:15 -0800

    1 file modified

--- app/public.component.ts
+++ app/public.component.ts
@@ -3,7 +3,7 @@ import { MatDialogRef, MatDialog } from '@angular/material';
 
 @Component({
   selector: 'public-component',
-  templateUrl: './pub.component.html'
+  templateUrl: './publc.component.html'
 })
 export class PublicComponent {
   constructor(private dialog: MatDialog){}
