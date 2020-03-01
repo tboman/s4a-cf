276013d26361a055d0cdb2e3c516c1746d52d0ab
tboman <tboman@ionetix.com>
2020-03-01 08:21:43 -0800

    1 file modified

--- app/public.component.ts
+++ app/public.component.ts
@@ -1,28 +1,9 @@
 import { Component } from '@angular/core';
-import { MatDialogRef, MatDialog } from '@angular/material';
 
 @Component({
   selector: 'public-component',
   templateUrl: './public.component.html'
 })
 export class PublicComponent {
-  constructor(private dialog: MatDialog){}
-  confirmGoBack() {
-    this.dialog.open(ConfirmGoBackDialog, {disableClose: true});
-  }
-}
-
-@Component({
-  selector: 'confirm-go-back-dialog',
-  templateUrl: './confirm.dialog.html'
-})
-export class ConfirmGoBackDialog {
-  constructor(private dialogRef: MatDialogRef<ConfirmGoBackDialog>){}
-  cancel() {
-    this.dialogRef.close();
-  }
-  goBack() {
-    this.dialogRef.close();
-    window.location.href = "https://chan4077-material2-docs.firebaseapp.com";
-  }
+  constructor(){}
 }
