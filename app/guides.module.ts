904c554747f646b2dbfc3e04fc23e39c77233b0a
tboman <tboman@ionetix.com>
2020-03-01 00:08:03 -0800

    1 file modified, 3 added

--- app/guides.module.ts
+++ app/guides.module.ts
@@ -5,7 +5,7 @@ import { MaterialModule } from './material.module';
 import { FormsModule } from '@angular/forms';
 import { ExampleModule } from './example.module';
 import { GettingStartedGuide } from '../guides/africa';
-import { WestGuide } from '../guides/west';
+import { WestSignup } from '../guides/signup-west';
 
 @NgModule({
   declarations: [
