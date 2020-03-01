31958a8966c7b86a5c6060218439ae16d450bf5e
tboman <tboman@ionetix.com>
2020-03-01 00:04:45 -0800

    2 files added

---
+++ guides/west.ts
@@ -0,0 +1,47 @@
+export * from './west.guide';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { 
+  MatInputModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule,
+  MatSelectModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatSlideToggleModule,
+  MatSliderModule,
+  MatDatepickerModule } from '@angular/material';
+
+import { AppComponent } from '../app/app.component';
+import { InputComponent } from '../app/input/input.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatDatepickerModule ],
+  declarations: [ 
+    AppComponent,
+    InputComponent
+    ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
