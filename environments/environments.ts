02432846484253dc09fd267fbd5354bbb3c7befb
tboman <tboman@ionetix.com>
2020-03-03 22:13:59 -0800

    1 file modified

--- environments/environments.ts
+++ environments/environments.ts
@@ -4,7 +4,7 @@ export const environment = {
       apiKey: "AIzaSyAnyTHp1y0mUjQbFTsBShbP_qcaCLaVzG8",
       authDomain: "bhazze.com",
       databaseURL: "DATABASE-URL-HERE",
-      projectId: "PROJECT-ID-HERE",
+      projectId: "project-437005760912",
       storageBucket: "STORAGE-BUCKET-HERE",
       messagingSenderId: "MESSAGING-SENDER-ID-HERE"
    }
