b92c99e020388084467936deb71f67ecb0f9ecd9
tboman <tboman@ionetix.com>
2020-03-04 22:34:15 -0800

    1 file modified

--- app/home.component.ts
+++ app/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase/app";
 
 @Component({
-  selector: 'home-component',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "home-component",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    const db = firebase.firestore();
+    db.settings({ timestampsInSnapshots: true });
+    db.collection("requests")
+      .get()
+      .then(snapshot => {
+        snapshot.docs.forEach(doc => {
+          console.log(doc.data);
+        });
+      });
   }
-
 }
