6882016a59c3599c9e98b01359a664418c7baa09
tboman <tboman@ionetix.com>
2020-03-04 22:22:13 -0800

    1 file modified

--- environments/environments.ts
+++ environments/environments.ts
@@ -1,11 +1,13 @@
 export const environment = {
-   production: false,
-   firebase: {
-      apiKey: "AIzaSyAnyTHp1y0mUjQbFTsBShbP_qcaCLaVzG8",
-      authDomain: "s4a.bhazze.com",
-      databaseURL: "DATABASE-URL-HERE",
-      projectId: "science4africa-e9a07",
-      storageBucket: "STORAGE-BUCKET-HERE",
-      messagingSenderId: "MESSAGING-SENDER-ID-HERE"
-   }
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAnyTHp1y0mUjQbFTsBShbP_qcaCLaVzG8",
+    authDomain: "s4a.bhazze.com",
+    databaseURL: "https://science4africa-e9a07.firebaseio.com",
+    projectId: "science4africa-e9a07",
+    storageBucket: "science4africa-e9a07.appspot.com",
+    messagingSenderId: "437005760912",
+    appId: "1:437005760912:web:620ebed878b91c3769db15",
+    measurementId: "G-D5SED8B6QK"
+  }
 };
