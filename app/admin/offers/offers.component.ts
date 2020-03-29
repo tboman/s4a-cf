feb3d7106c908ca46df60798478719a33da871f6
tboman <tboman@ionetix.com>
2020-03-28 22:25:43 -0700

    Adding admin view of all offers

--- app/admin/offers/offers.component.ts
+++ app/admin/offers/offers.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase/app";
+import { Offer } from "../../model/offer";
+import { CacheService } from "../../../services/cache.service";
 
 @Component({
-  selector: 'offers',
-  templateUrl: './offers.component.html',
-  styleUrls: ['./offers.component.css']
+  selector: "offers",
+  templateUrl: "./offers.component.html",
+  styleUrls: ["./offers.component.css"]
 })
 export class OffersComponent implements OnInit {
+  offers = [];
+  interests;
 
-  constructor() { }
+  constructor(private cacheService: CacheService) {
+    const user = firebase.auth().currentUser;
+    const db = firebase.firestore();
+    db.collection("offers")
+      .get()
+      .then(snapshot => {
+        snapshot.docs.forEach(doc => {
+          this.renderOffer(doc);
+        });
+      });
 
-  ngOnInit() {
+    this.interests = cacheService.getInterests();
   }
 
+  ngOnInit() {}
+
+  renderOffer(doc) {
+    var offer: Offer = new Offer();
+    offer.email = doc.data().email;
+    offer.title = doc.data().title;
+    offer.name = doc.data().name;
+    offer.work = doc.data().work;
+    offer.field = doc.data().field;
+    offer.need = doc.data().need;
+    offer.location = doc.data().location;
+    offer.created = doc.data().created.toDate();
+    offer.interest = this.interests.find(
+      interest => interest.key === doc.data().interest
+    ).value;
+    this.offers.push(offer);
+  }
 }
